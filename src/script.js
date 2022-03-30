66b7048dc726dff4e8a5c92c6a891ab5d8afbf30
Kawika Mendiola <kwkmendiolabusiness@gmail.com>
2022-03-29 21:07:26 -0700

    Added textgeometry/matcaps

--- src/script.js
+++ src/script.js
@@ -3,7 +3,9 @@ import "./css/style.min.css"
 import gsap from "gsap"
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import { TextureLoader } from "three/examples/jsm/loaders/BasisTextureLoader"
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"
+import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json"
 import * as dat from "dat.gui"
 // Debug
 const gui = new dat.GUI()
@@ -13,44 +15,40 @@ const canvas = document.querySelector(".webgl")
 // const ctx = canvas.getContext("2d")
 // ctx.fillStyle = "green"
 // ctx.fillRect(10, 10, 150, 100)
-
-const container = document.querySelector(".container")
-// Scene
+/**
+ * Scene
+ */
 const scene = new THREE.Scene()
 
-// Sphere Group
-// const sphereGroup = new THREE.Group();
-// scene.add(sphereGroup);
-// const sphere = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.3, 32, 16),
-//   new THREE.MeshBasicMaterial({ color: 0x34e8eb })
-// );
-// sphereGroup.add(sphere);
-// const sphere2 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.3, 32, 16),
-//   new THREE.MeshBasicMaterial({ color: 0xffffff })
-// );
-// sphere2.position.x = -1;
-// sphereGroup.add(sphere2);
+/**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const donutTexture = textureLoader.load("/matcaps/8.png")
+const textTexture = textureLoader.load("/matcaps/3.png")
+/**
+ * Objects
+ */
 
 // Cube Group
-const cubeGroup = new THREE.Group()
-scene.add(cubeGroup)
+// const cubeGroup = new THREE.Group()
+// scene.add(cubeGroup)
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1.5, 0.5, 1),
-  new THREE.MeshBasicMaterial({ color: 0xbfecff })
-)
-cube.position.x = 3
-// cube.material.opacity = 0;
-cubeGroup.add(cube)
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1.5, 0.5, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xbfecff })
+// )
+// cube.position.x = 3
+// // cube.material.opacity = 0;
+// cubeGroup.add(cube)
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1.5, 0.5, 1),
-  new THREE.MeshBasicMaterial({ color: 0xa1eeff })
-)
-cube2.position.x = -3
-cubeGroup.add(cube2)
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1.5, 0.5, 1),
+//   new THREE.MeshBasicMaterial({ color: 0xa1eeff })
+// )
+// cube2.position.x = -3
+// cubeGroup.add(cube2)
 
 // const cube3 = new THREE.Mesh(
 //   new THREE.BoxGeometry(1.5, 0.5, 1),
@@ -59,13 +57,68 @@ cubeGroup.add(cube2)
 // cube3.position.x = 3;
 // cubeGroup.add(cube3);
 
-// Lights
+// // Floor
+// const plane = new THREE.Mesh(
+//   new THREE.PlaneGeometry(25, 25),
+//   new THREE.MeshStandardMaterial({ color: "green" })
+// )
+// plane.rotation.x = 5
+// plane.position.y = -1
+// scene.add(plane)
+// gui.add(plane.rotation, "x").min(0).max(15).step(0.1)
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textMat = new THREE.MeshMatcapMaterial({ matcap: textTexture })
+
+  // Text
+  const textGeometry = new TextGeometry("Kawika Mendiola", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  })
+  textGeometry.center()
+
+  const text = new THREE.Mesh(textGeometry, textMat)
+  scene.add(text)
+
+  // Donuts
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+  const donutMat = new THREE.MeshMatcapMaterial({ matcap: donutTexture })
+  for (let i = 0; i < 1000; i++) {
+    const donut = new THREE.Mesh(donutGeometry, donutMat)
+    donut.position.x = (Math.random() - 0.5) * 20
+    donut.position.y = (Math.random() - 0.5) * 20
+    donut.position.z = (Math.random() - 0.5) * 20
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+    scene.add(donut)
+  }
+})
+/**
+ * Lights
+ */
 
 const pointLight = new THREE.PointLight(0xffffff, 0.1)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0x404040, 1)
+scene.add(ambientLight)
 
 /**
  * Sizes
@@ -99,21 +152,26 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 )
-camera.position.z = 5
-camera.position.y = 1
 
+camera.position.y = 2
+camera.position.z = 3
+gui.add(camera.position, "x", 0, 10, 0.1).name("cam pos x")
+gui.add(camera.position, "y", 0, 10, 0.1).name("cam pos y")
+gui.add(camera.position, "z", 0, 10, 0.1).name("cam pos z")
+
+gui.add(camera.rotation, "x", -5, 5, 0.001).name("cam rotate x")
+camera.rotation.x = -1.1
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas);
-// controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  alpha: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -128,14 +186,9 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  // sphere.rotation.y = 0.5 * elapsedTime;
-  // sphere.rotation.x = 0.5 * elapsedTime;
-  // sphereGroup.rotation.y = Math.PI * elapsedTime;
-  // sphere2.rotation.x = 0.5 * elapsedTime;
+  // cube.rotation.y = 1 * elapsedTime
 
-  cube.rotation.y = 1 * elapsedTime
-
-  cube2.rotation.y = 1 * elapsedTime
+  // cube2.rotation.y = 1 * elapsedTime
 
   // cube3.rotation.y = 1 * elapsedTime;
 
