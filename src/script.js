24438d49b7ae03a6cfb1b8d337f49f5406efa813
Kawika Mendiola <kwkmendiolabusiness@gmail.com>
2022-04-03 04:34:51 -0700

    Added a textures folder and starting scroll-based animation.

--- src/script.js
+++ src/script.js
@@ -7,216 +7,103 @@ import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"
 import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json"
 import * as dat from "dat.gui"
-// Debug
-const gui = new dat.GUI()
 
-// Canvas
-const canvas = document.querySelector(".webgl")
-// const ctx = canvas.getContext("2d")
-// ctx.fillStyle = "green"
-// ctx.fillRect(10, 10, 150, 100)
+const gui = new dat.GUI()
+const parameters = {
+  materialColor: "#ffeded",
+}
+
+gui.addColor(parameters, "materialColor").onChange(() => {
+  particlesMaterial.color.set(parameters.materialColor)
+  material.color.set(parameters.materialColor)
+})
+
 /**
- * Scene
+ * Base
  */
+// Canvas
+const canvas = document.querySelector("canvas.webgl")
+
+// Scene
 const scene = new THREE.Scene()
 
 /**
  * Textures
  */
-
 const textureLoader = new THREE.TextureLoader()
-const donutTexture = textureLoader.load("/matcaps/8.png")
-const textTexture = textureLoader.load("/matcaps/3.png")
+const gradientTexture = textureLoader.load("textures/gradients/3.jpg")
+gradientTexture.magFilter = THREE.NearestFilter
 /**
  * Objects
  */
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+})
+/**
+ * Meshes
+ */
+const objectsDistance = 4
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material)
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material)
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
+)
+mesh1.position.y = -objectsDistance * 0
+mesh2.position.y = -objectsDistance * 1
+mesh3.position.y = -objectsDistance * 2
 
-// Cube Group
-// const cubeGroup = new THREE.Group()
-// scene.add(cubeGroup)
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
+scene.add(mesh1, mesh2, mesh3)
 
-// const cube = new THREE.Mesh(
-//   new THREE.BoxGeometry(1.5, 0.5, 1),
-//   new THREE.MeshBasicMaterial({ color: 0xbfecff })
-// )
-// cube.position.x = 3
-// // cube.material.opacity = 0;
-// cubeGroup.add(cube)
-
-// const cube2 = new THREE.Mesh(
-//   new THREE.BoxGeometry(1.5, 0.5, 1),
-//   new THREE.MeshBasicMaterial({ color: 0xa1eeff })
-// )
-// cube2.position.x = -3
-// cubeGroup.add(cube2)
-
-// const cube3 = new THREE.Mesh(
-//   new THREE.BoxGeometry(1.5, 0.5, 1),
-//   new THREE.MeshBasicMaterial({ color: 0xa1eeff })
-// );
-// cube3.position.x = 3;
-// cubeGroup.add(cube3);
-
-// // Floor
-// const plane = new THREE.Mesh(
-//   new THREE.PlaneGeometry(25, 25),
-//   new THREE.MeshStandardMaterial({ color: "green" })
-// )
-// plane.rotation.x = 5
-// plane.position.y = -1
-// scene.add(plane)
-// gui.add(plane.rotation, "x").min(0).max(15).step(0.1)
-
-// const object1 = new THREE.Mesh(
-//   new THREE.BoxGeometry(0.25, 0.25),
-//   new THREE.MeshBasicMaterial({ color: "#ff0000" })
-// )
-// object1.position.x = -2
-
-// const object2 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.5, 16, 16),
-//   new THREE.MeshBasicMaterial({ color: "#ff0000" })
-// )
-
-// const object3 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.5, 16, 16),
-//   new THREE.MeshBasicMaterial({ color: "#ff0000" })
-// )
-// object3.position.x = 2
-
-// scene.add(object1, object2, object3)
-
-// /**
-//  * Raycaster
-//  */
-// const raycaster = new THREE.Raycaster()
-// let currentIntersect = null
-// const rayOrigin = new THREE.Vector3(-3, 0, 0)
-// const rayDirection = new THREE.Vector3(10, 0, 0)
-// rayDirection.normalize()
+const sectionMeshes = [mesh1, mesh2, mesh3]
 
 /**
  * Particles
  */
+
 // Geometry
-const particleTexture = textureLoader.load("/particles/9.png")
-const particlesGeometry = new THREE.BufferGeometry()
-const count = 10000
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
 
-const positions = new Float32Array(count * 3)
-const colors = new Float32Array(count * 3)
-
-for (let i = 0; i < count * 3; i++) {
-  positions[i] = (Math.random() - 0.5) * 25
-  colors[i] = Math.random()
+for (let i = 0; i < particlesCount; i++) {
+  // x
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+  // y
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 -
+    Math.random() * objectsDistance * sectionMeshes.length
+  // z
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10
 }
 
+const particlesGeometry = new THREE.BufferGeometry()
 particlesGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(positions, 3)
 )
-particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3))
 
 // Material
-const particlesMaterial = new THREE.PointsMaterial()
-
-particlesMaterial.size = 0.15
-particlesMaterial.sizeAttenuation = true
-
-particlesMaterial.color = new THREE.Color("#ff88cc")
-
-particlesMaterial.transparent = true
-particlesMaterial.alphaMap = particleTexture
-// particlesMaterial.alphaTest = 0.01
-// particlesMaterial.depthTest = false
-particlesMaterial.depthWrite = false
-particlesMaterial.blending = THREE.AdditiveBlending
-
-particlesMaterial.vertexColors = true
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+})
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
-/**
- * Fonts
- */
-const textObj = {
-  text1: null,
-  aboutText: null,
-}
-const fontLoader = new FontLoader()
-fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  const textMat = new THREE.MeshMatcapMaterial({ matcap: textTexture })
 
-  // Text
-  const textGeometry = new TextGeometry("Kawika Mendiola", {
-    font: font,
-    size: 0.8,
-    height: 0.2,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 0.03,
-    bevelSize: 0.02,
-    bevelOffset: 0,
-    bevelSegments: 5,
-  })
-  textGeometry.center()
-
-  const text1 = new THREE.Mesh(textGeometry, textMat)
-  scene.add(text1)
-
-  const textGeometry2 = new TextGeometry("About", {
-    font: font,
-    size: 0.3,
-    height: 0.2,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 0.03,
-    bevelSize: 0.002,
-    bevelOffset: 0,
-    bevelSegments: 5,
-  })
-  textGeometry.center()
-
-  const aboutText = new THREE.Mesh(textGeometry2, textMat)
-  aboutText.position.x = -3.07
-  aboutText.position.y = -0.9
-  aboutText.position.z = 0.18
-  aboutText.rotation.y = 0.18
-  scene.add(aboutText)
-  gui.add(aboutText.position, "x", -5, 5, 0.01).name("About pos x")
-  gui.add(aboutText.position, "y", -5, 5, 0.01).name("About pos y")
-  gui.add(aboutText.position, "z", -5, 5, 0.01).name("About pos z")
-  gui.add(aboutText.rotation, "x", -5, 5, 0.01).name("About rot x")
-  gui.add(aboutText.rotation, "y", -5, 5, 0.01).name("About rot y")
-  console.log(aboutText)
-  // Donuts
-  // const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
-  // const donutMat = new THREE.MeshMatcapMaterial({ matcap: donutTexture })
-  // for (let i = 0; i < 1000; i++) {
-  //   const donut = new THREE.Mesh(donutGeometry, donutMat)
-  //   donut.position.x = (Math.random() - 0.5) * 20
-  //   donut.position.y = (Math.random() - 0.5) * 20
-  //   donut.position.z = (Math.random() - 0.5) * 20
-  //   donut.rotation.x = Math.random() * Math.PI
-  //   donut.rotation.y = Math.random() * Math.PI
-  //   const scale = Math.random()
-  //   donut.scale.set(scale, scale, scale)
-  //   scene.add(donut)
-  // }
-})
 /**
  * Lights
  */
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 
-const ambientLight = new THREE.AmbientLight(0x404040, 1)
-scene.add(ambientLight)
-
+const sun = new THREE.DirectionalLight("#ffffff", 1)
+sun.position.set(1, 1, 0)
+scene.add(sun)
 /**
  * Sizes
  */
@@ -242,98 +129,91 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  35,
   sizes.width / sizes.height,
   0.1,
   100
 )
-
-camera.position.y = 0
-camera.position.z = 3.9
-gui.add(camera.position, "x", 0, 10, 0.1).name("cam pos x")
-gui.add(camera.position, "y", 0, 10, 0.1).name("cam pos y")
-gui.add(camera.position, "z", 0, 10, 0.1).name("cam pos z")
-
-gui.add(camera.rotation, "x", -5, 5, 0.001).name("cam rotate x")
-camera.rotation.x = -1.1
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+camera.position.z = 6
+cameraGroup.add(camera)
+scene.add(cameraGroup)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY
+let currentSection = 0
+
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY
+
+  const newSection = Math.round(scrollY / sizes.height)
+
+  if (newSection != currentSection) {
+    currentSection = newSection
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.inOut",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    })
+  }
+})
+
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+}
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5
+  cursor.y = e.clientY / sizes.height - 0.5
+})
 
 /**
  * Animate
  */
-
 const clock = new THREE.Clock()
-// const mouse = new THREE.Vector2()
-
-// window.addEventListener("mousemove", (event) => {
-//   mouse.x = (event.clientX / sizes.width) * 2 - 1
-//   mouse.y = -(event.clientY / sizes.height) * 2 + 1
-// })
-
-// window.addEventListener("click", () => {
-//   if (currentIntersect) {
-//     switch (currentIntersect.object) {
-//       case object1:
-//         console.log("click on object 1")
-//         break
-
-//       case object2:
-//         console.log("click on object 2")
-//         break
-
-//       case object3:
-//         console.log("click on object 3")
-//         break
-//     }
-//   }
-// })
-
+let previousTime = 0
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
-  // Update objects
-  // cube.rotation.y = 1 * elapsedTime
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-  // cube2.rotation.y = 1 * elapsedTime
+  // Animate camera
+  camera.position.y = (-scrollY / sizes.height) * objectsDistance
 
-  // cube3.rotation.y = 1 * elapsedTime;
-  // Cast a ray from the mouse and handle events
-  // raycaster.setFromCamera(mouse, camera)
+  const parallaxX = cursor.x * 0.5
+  const parallaxY = -cursor.y * 0.5
+  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
-  // const objectsToTest = [object1, object2, object3]
-  // const intersects = raycaster.intersectObjects(objectsToTest)
+  // Animate meshes
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x += deltaTime * 0.1
+    mesh.rotation.y += deltaTime * 0.12
+  }
 
-  // if (intersects.length) {
-  //   if (!currentIntersect) {
-  //     console.log("mouse enter")
-  //   }
-
-  //   currentIntersect = intersects[0]
-  // } else {
-  //   if (currentIntersect) {
-  //     console.log("mouse leave")
-  //   }
-
-  //   currentIntersect = null
-  // }
-
-  // Update Orbital Controls
-  // controls.update()
-
+  // Animate particles
+  particles.rotation.y = 0.02 * elapsedTime
   // Render
   renderer.render(scene, camera)
 
@@ -342,7 +222,6 @@ const tick = () => {
 }
 
 tick()
-
 // Circle cursor
 const $bigBall = document.querySelector(".cursor__ball--big")
 const $smallBall = document.querySelector(".cursor__ball--small")
